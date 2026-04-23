4cd5df52c071ec9664135456ef75d12ecbcbc2f3
Варяя <varyamerkulovaaa@gmail.comm>
2026-04-23 09:53:15 +0700

    Main functions

---
+++ app.py
@@ -0,0 +1,14 @@
+import json
+
+DATA_FILE = "students.json"
+
+def load_data():
+    try:
+        with open(DATA_FILE, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {"students": [], "next_id": 1}
+
+def save_data(data):
+    with open(DATA_FILE, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
