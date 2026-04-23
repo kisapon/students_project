0d7aa17f264f9cca8ad76eb8ab6ce2fe05a00fc2
Варяя <varyamerkulovaaa@gmail.comm>
2026-04-23 11:14:42 +0700

    Result

--- app.py
+++ app.py
@@ -12,3 +12,34 @@ def load_data():
 def save_data(data):
     with open(DATA_FILE, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+from function1 import *
+from function2 import *
+from function3 import *
+from function4 import *
+
+print("Приложение по работе со студентами.")
+while True:
+    print("\nФункционал:")
+    print("1 -> Добавление нового студента.")
+    print("2 -> Просмотр всех студентов.")
+    print("3 -> Просмотр одного студента, включая его средний балл.")
+    print("4 -> Удаление студента.")
+    print("5 -> Выход.")
+
+    choice = input("Выберите функцию: ")
+
+    if choice == '1':
+        add_student()
+    elif choice == '2':
+        view_all_students()
+    elif choice == '3':
+        view_student()
+    elif choice == '4':
+        delete_student()
+    elif choice == '5':
+        print("До свидания!")
+        break
+    else:
+        print("Такой функции нет! Попробуйте снова.")
